08d7b55b2e0b6e43936ce3b62a662262100086f5
nicolascal <nicolasfcal@gmail.com>
2018-04-23 15:53:19 -0300

    Se maveniza proyecto y se agrega dependencia gson

---
+++ dds-g13-mie-tn/src/main/java/ar/utn/frba/dds/g13/App.java
@@ -0,0 +1,13 @@
+package ar.utn.frba.dds.g13;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
